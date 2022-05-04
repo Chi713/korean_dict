f608db70a804d8de46c8b39b96956aa6eb9b2f9d
Chi713 <chris.hoskin2355@gmail.com>
2022-05-04 15:41:45 -0400

    parsed XML and returns dict

--- index.py
+++ index.py
@@ -1,4 +1,15 @@
 from khaiii import KhaiiiApi
-api = KhaiiiApi()
-for word in api.analyze('안녕, 세상.'):
-    print(word)
+import koreanDict as krdict
+
+#api = KhaiiiApi()
+#for word in api.analyze('안녕, 세상.'):
+#    print(word)
+
+f = open(".apikey")
+API_KEY = f.read().strip()
+f.close()
+
+query = "나무"
+
+entry = krdict.search(API_KEY,query)
+print(entry)
--- koreanDict.py
+++ koreanDict.py
@@ -1,21 +1,38 @@
 import requests
 import certifi
+import xml.etree.ElementTree as ET
 
 URL = "https://krdict.korean.go.kr/api/search?"
 
-f = open(".apikey")
-API_KEY = f.read().strip()
-f.close()
-query = "나무"
+#f = open(".apikey")
+#API_KEY = f.read().strip()
+#f.close()
 
-#print(API_KEY)
-PARAMS = {'key': API_KEY,
-    'q': query,
-    'translated': 'y',
-    'trans_lang': '1'}
+#theQuery = "나무"
 
-r = requests.get(url = URL, params = PARAMS, verify=certifi.where())
+def search(key,query):
+    PARAMS = {'key': key,
+        'q': query,
+        'translated': 'y',
+        'trans_lang': '1'}
 
-#r = http.request('GET', URL)
-#print(r.data)
-print(r.content)
+    r = requests.get(url = URL, params = PARAMS, verify=certifi.where())
+    data = r.content.decode('UTF-8').strip()
+    root = ET.fromstring(data)
+    word = root[7][1].text
+    defi = []
+    expl = []
+
+    for child in root[7]:
+        if child.tag == 'sense':
+            defi.append(child[2][1].text)
+            expl.append(child[2][2].text)
+
+    #print(word)
+    #for i in range(len(defi)):
+        #print(defi[i],expl[i])
+        
+    entry = {'word': word,
+        'definition': defi,
+        'explaination': expl}
+    return entry
