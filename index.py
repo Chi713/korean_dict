079f9280e22478745e89d3d50d4e5add4fee94ad
Chi713 <chris.hoskin2355@gmail.com>
2022-05-11 17:29:50 -0400

    make api calls async

--- index.py
+++ index.py
@@ -2,24 +2,40 @@ import koreanDict as krdict
 import parser as parser
 import pprint
 import os
+import asyncio
+import time
 
-#f = open(".apikey")
-#API_KEY = f.read().strip()
-#f.close()
-API_KEY = os.environ['API_KEY']
+if "API_KEY" in os.environ:
+    API_KEY = os.environ['API_KEY']
+else:
+    f = open(".apikey")
+    API_KEY = f.read().strip("API_KEY=").strip()
+    f.close()
+    #print(API_KEY)
 
 query = "나무"
-sentence = '제 친구 정우가 공항에서 저와 줄리아를 기다리고 있었어요.'
+#sentence = '제 친구 정우가 공항에서 저와 줄리아를 기다리고 있었어요.'
 sentence = '어절 내에 포함된 형태소의 리스트를 생성한다'
 entry = []
 
 data = parser.parse(sentence)
 
-for i in range(len(data['morpheme'])):
-    word = krdict.search(API_KEY,data['morpheme'][i])
-    entry.append(word)
+async def main():
+    #entry.append(await krdict.search(API_KEY, query))
+    for (morph, tag) in zip(data['morpheme'], data['tag']):
+        word = await krdict.search(API_KEY,morph)
+        global entry
+        entry.append(word)
+        print(word)
 
-print('\nsentence:',sentence)
-print('morphemes:',data['morpheme'],'\n')
-entry.append(krdict.search(API_KEY,query))
-pprint.pprint(entry)
+    #print('\nsentence:',sentence)
+    #print('morphemes:',data['morpheme'],'\n')
+    #entry.append(krdict.search(API_KEY,query))
+    pprint.pprint(entry)
+    pprint.pprint(data['tag'])
+tic = time.perf_counter()
+asyncio.run(main())
+toc = time.perf_counter()
+
+timePass = toc-tic
+print(timePass)
--- koreanDict.py
+++ koreanDict.py
@@ -1,53 +1,71 @@
-import requests
+#import requests
 import certifi
 import xml.etree.ElementTree as ET
-import time
-#import urllib3
+#import time
+import ssl
+#import asyncio
+import aiohttp
+
+#query = "나무"
+
+#f = open(".apikey")
+#API_KEY = f.read().strip("API_KEY=").strip()
+#f.close()
 
 BASE_URL = "https://krdict.korean.go.kr/api/search?"
-#http = urllib3.PoolManager(
-    #cert_reqs='CERT_REQUIRED',
-    #ca_certs=certifi.where())
 
-def search(key,query):
+sslcontext = ssl.create_default_context(
+    cafile = certifi.where())
+
+async def fetch(session, params):
+    global BASE_URL
+    global sslcontext
+    async with session.get(BASE_URL, params=params, ssl=sslcontext) as r:
+        return await r.read()
+
+async def search(key,query):
+
     PARAMS = {'key': key,
         'q': query,
         'translated': 'y',
         'trans_lang': '1'}
-   
-    #URL = BASE_URL + "key=" + key + "&q=" + query + "&translated=y&trans_lang=1"
-    #print(URL)
-    #r = http.request('GET', URL)
+    
+    async with aiohttp.ClientSession() as session:
+        data = await fetch(session, PARAMS)
 
-    tic = time.perf_counter()
-    r = requests.get(url = BASE_URL, params = PARAMS, verify=certifi.where())
-    toc = time.perf_counter()
 
-    if r.status_code != 200:
-        print("error in fetching data from krdict.korean.go.kr/api")
-        return r.raise_for_status
+    #tic = time.perf_counter()
+    #r = requests.get(url = BASE_URL, params = PARAMS, verify=certifi.where())
+    #toc = time.perf_counter()
 
-   #data = r.data.decode('UTF-8').strip()
-    data = r.content.decode('UTF-8').strip()
-    root = ET.fromstring(data)
-    word = ""
-    defi = []
-    expl = []
-    flag = True
+    #if r.status_code != 200:
+        #print("error in fetching data from krdict.korean.go.kr/api")
+        #return r.raise_for_status
 
-    for branch in root[7:]:
-        for middle in [x for x in branch if x.tag == 'sense' and flag]:
-            for child in [x for x in middle if x.tag == 'translation']:
-                word = branch[1].text
-                defi.append(str(child[1].text).strip())
-                expl.append(str(child[2].text).strip())
-                flag = False
+        #data = str(r.read()).decode('UTF-8').strip()
+        root = ET.fromstring(data)
+        word = ""
+        defi = []
+        expl = []
+        flag = True
 
-    entry = {'word': word,
-        'definition': defi,
-        'explaination': expl}
+        for branch in root[7:]:
+            for middle in [x for x in branch if x.tag == 'sense' and flag]:
+                for child in [x for x in middle if x.tag == 'translation']:
+                    word = branch[1].text
+                    defi.append(str(child[1].text).strip())
+                    expl.append(str(child[2].text).strip())
+                    flag = False
+
+        entry = {'word': word,
+            'definition': defi,
+            'explaination': expl}
         
-    timePass = toc-tic
-    print("time for request:",timePass)
+        #print(entry)
 
-    return entry 
+    #timePass = toc-tic
+    #print("time for request:",timePass)
+
+#asyncio.run(search(API_KEY, query))
+
+        #return entry 
