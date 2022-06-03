01d2aaea86939101769fff448f0da1bebde322f9
Chi713 <chris.hoskin2355@gmail.com>
2022-06-03 10:19:00 -0400

    added komoran to parser and changed parser api

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,18 @@
-use korean_dict::parser::Parser;
+use korean_dict::parser::{Parser, ParserKind, Sentence};
 use korean_dict::search::Session;
 use std::time::Instant;
 
 #[tokio::main]
 async fn main() {
     let now = Instant::now();
-    let sentence = "안녕, 새상.".to_owned();
+    let test_sentence = "안녕, 새상.".to_owned();
     let query = "나무".to_owned();
     let client = Session::new().await;
-    let parse = Parser::new();
+    let sentence = Sentence::new(test_sentence.clone());
+    let parser = Parser::new();
+    let parser = parser.change_parser(ParserKind::Khaiii);
 
-    let res = parse.parse(sentence).unwrap();
+    let res = sentence.parse(&parser).unwrap();
     println!("parsed sentence: {:?}", res);
 
     let response = client.get(query).await.unwrap();
--- src/parser.rs
+++ src/parser.rs
@@ -1,24 +1,61 @@
 use pyo3::prelude::*;
-//use pyo3::types::PyList;
-//use pyo3::types::PyTuple;
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy)]
+pub enum ParserKind {
+    Khaiii,
+    Komoran,
+}
+
+impl Default for ParserKind {
+    fn default() -> Self {
+        ParserKind::Komoran
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
 pub struct Parser {
-    pub parser: String,
+    parser: ParserKind,
 }
 
 impl Parser {
-    pub fn new() -> Parser {
-        Parser {
-            parser: "khaiii".to_owned(),
+    pub fn new() -> Self {
+        Self {
+            parser: ParserKind::default(),
         }
     }
 
-    pub fn parse(&self, sentence: String) -> PyResult<Vec<Vec<(String, String)>>> {
+    pub fn parse(self, sentence: Sentence) -> PyResult<Vec<(String, String)>> {
+        match self.parser {
+            ParserKind::Khaiii => self.khaiii_parse(sentence),
+            ParserKind::Komoran => self.komoran_parse(sentence),
+        }
+    }
+
+    fn komoran_parse(self, sentence: Sentence) -> PyResult<Vec<(String, String)>> {
+        let res: PyResult<Vec<(String, String)>> = Python::with_gil(|py| {
+            let fun = PyModule::from_code(
+                py,
+                "def parse(*args):
+                from konlpy.tag import Komoran
+                return Komoran().pos(args[0])",
+                "",
+                "",
+            )?;
+
+            let result = fun
+                .getattr("parse")?
+                .call1((sentence.sentence,))?
+                .extract()?;
+            Ok(result)
+        });
+        Ok(res?)
+    }
+
+    fn khaiii_parse(self, sentence: Sentence) -> PyResult<Vec<(String, String)>> {
         let res: PyResult<Vec<Vec<(String, String)>>> = Python::with_gil(|py| {
             let fun = PyModule::from_code(
                 py,
-                "def parse(*args, **kwargs):
+                "def parse(*args):
                 from khaiii import KhaiiiApi
                 data = KhaiiiApi().analyze(args[0])
 
@@ -34,11 +71,78 @@ impl Parser {
 
             let result = fun
                 .getattr("parse")?
-                .call1((sentence,))?
-                .extract::<Vec<Vec<(String, String)>>>()?;
+                .call1((sentence.sentence,))?
+                .extract()?;
 
             Ok(result)
         });
-        Ok(res?)
+        //let res = res?;
+        //temporary fix to get code to run. TODO need to do actual parsing
+        let mut words: Vec<(String, String)> = Vec::new();
+
+        res?.into_iter().for_each(|sent| {
+            println!("{:?}", sent);
+            words.push(sent[0].clone());
+        });
+
+        Ok(words)
+    }
+
+    //slient error being handled here MAKE SURE TO FIX
+    pub fn change_parser(mut self, parser: ParserKind) -> Self {
+        let flag = Self::check_khaiii_install();
+        let parser = match (parser, flag) {
+            (ParserKind::Khaiii, true) => ParserKind::Khaiii,
+            (ParserKind::Khaiii, false) => ParserKind::Komoran,
+            (_, _) => ParserKind::Komoran,
+        };
+        self.parser = parser;
+        self
+    }
+
+    pub fn check_khaiii_install() -> bool {
+        let res: PyResult<bool> = Python::with_gil(|py| {
+            let check = PyModule::from_code(
+                py,
+                "def check(*args):
+                try:
+                    import khaiii
+                    return True
+                except ImportError:
+                    return False",
+                "",
+                "",
+            )?;
+            let result = check.getattr("check")?.call0()?.extract()?;
+            Ok(result)
+        });
+        println!("Khaiii installed?: {}", res.as_ref().unwrap());
+        res.unwrap()
+    }
+
+    pub fn parser_type(&self) -> ParserKind {
+        println!("{:?}", self.parser);
+        self.parser
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct Sentence {
+    sentence: String,
+}
+
+impl Sentence {
+    pub fn new(sentence: String) -> Self {
+        Self { sentence }
+    }
+
+    pub fn parse(self, parser: &Parser) -> PyResult<Vec<(String, String)>> {
+        //do I want to handle match statement here or in Parser Struct
+        match parser.parser {
+            ParserKind::Khaiii => parser.khaiii_parse(self),
+            ParserKind::Komoran => parser.komoran_parse(self),
+        }
+        //passes logic to Parser Struct
+        //parser.parse(self)
     }
 }
