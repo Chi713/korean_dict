ad9382c201f2bf83211d128a6c4f0ccfa7db9e11
Chi713 <chris.hoskin2355@gmail.com>
2023-03-18 00:08:20 -0400

    refactored FileUploader component, passed callback prop to it to return server data to main app component, and display data to the DOM

--- frontend/src/components/file_uploader.rs
+++ frontend/src/components/file_uploader.rs
@@ -1,11 +1,10 @@
 use yew::prelude::*;
 use yew::{
     Callback, 
-    Component, 
-    Context, 
     html::TargetCast, 
     Html,
     platform::spawn_local,
+    Properties
 };
 
 use web_sys::{
@@ -14,10 +13,7 @@ use web_sys::{
     HtmlInputElement
 };
 
-use gloo::file::{
-    callbacks::FileReader,
-    File,
-};
+use gloo::file::File;
 
 use std::collections::HashMap;
 use serde::{Serialize, Deserialize};
@@ -33,210 +29,184 @@ pub struct Entry {
 
 #[derive(Serialize, Deserialize, Debug, Clone, Default)]
 pub struct RecievedData {
-    sentence: Vec<Entry>,
-    prev_flag: bool,
-    next_flag: bool,
+    pub sentence: Vec<Entry>,
+    pub prev_flag: bool,
+    pub next_flag: bool,
 }
 
-#[derive(Clone, PartialEq)]
+#[derive(Clone, PartialEq, Debug)]
 struct FileDetails {
     name: String,
     data: String,
 }
 
-
-pub enum Msg {
-    Loaded(String, String),
-    Files(Vec<File>),
-    Sent,
-    Recieved(AttrValue),
-    Clear,
+#[derive(Clone, PartialEq)]
+pub struct Sentence {
+    pub id: usize,
+    pub text: String,
 }
 
-pub struct FileUploader {
-    readers: HashMap<String, FileReader>,
-    files: Vec<FileDetails>,
-    file_return_data: RecievedData,
+#[derive(Clone, PartialEq, Properties)]
+pub struct SentenceListProps {
+    //pub sentences: Vec<Sentence>,
+    pub sentences_cb: Callback<RecievedData>,
 }
+
+//impl SentenceListProps {
+//    pub fn send_prop() {
+
+//    }
+//}
 
 //------------------------------------------------------------------------------
 
-impl Component for FileUploader {
-    type Message = Msg;
-    type Properties = ();
+#[function_component(FileUploader)]
+pub fn file_component(props: &SentenceListProps) -> Html {
+    let files = use_state(Vec::new);
+    let file_data: UseStateHandle<Option<FileDetails>> = use_state(|| None);
+    let readers = use_state(|| None);
 
-    fn create(_ctx: &Context<Self>) -> Self {
-
-        Self {
-            readers: HashMap::default(),
-            files: Vec::default(),
-            file_return_data: RecievedData::default(),
-        }
-    }
-
-    fn update(&mut self, ctx: &Context<Self>, msg: Self::Message) -> bool {
-        match msg {
-            Msg::Loaded(file_name, data) => {
-                self.files.clear();
-                self.files.push(FileDetails {
-                    data,
-                    name: file_name.clone(),
-                });
-                self.readers.remove(&file_name);
-
-                true
-            }
-            Msg::Files(files) => {
-                for file in files.into_iter() {
-                    let file_name = file.name();
-
-                    let task = {
-                        let link = ctx.link().clone();
-                        let file_name = file_name.clone();
-
-                        gloo::file::callbacks::read_as_text(&file, move |res| {
-                            link.send_message(Msg::Loaded(
-                                file_name,
-                                //file_type,
-                                res.expect("failed to read file"),
-                            ))
-                        })
-                    };
-                    self.readers.insert(file_name, task);
-                }
-
-                true
-            }
-            Msg::Sent => {
-                
-                if !self.files.is_empty() {
-                    log::debug!("file sent to server");
-                    let upload_file_cb = ctx.link().callback(Msg::Recieved);
-                    Self::upload_file(self.files.clone(), upload_file_cb);
-                }
-
-                false
-            }
-            Msg::Recieved(data) => {
-                let json_data: RecievedData = serde_json::from_str(&data).unwrap();
-                self.file_return_data = json_data.clone();
-                log::info!("server response:\n{:?}", json_data);
-
-                true
-            }
-            Msg::Clear => {
-                self.files.clear();
-
-                true
-            }
-        }
-    }
-
-    fn view(&self, ctx: &Context<Self>) -> Html {
-
-        let onchange = {ctx.link().callback(move |e: Event| {
-            let input: HtmlInputElement = e.target_unchecked_into();
-            Self::on_file_change(input.files())
-        })};
-
-        let ondrop=ctx.link().callback(|event: DragEvent| {
-            event.prevent_default();
-            let files = event.data_transfer().unwrap().files();
-            Self::on_file_change(files)
-        });
-
-        let onclick = ctx.link().callback(move |_| { Msg::Sent });
- 
-        let clear_file = ctx.link().callback(move |_| { Msg::Clear });
-
-        html! {
-            <div id="wrapper">
-                
-                <p id="title">{ "Upload Your Files To The Cloud" }</p>
-                
-                <label for="file-upload">
-                    <div
-                        id="drop-container"
-                        ondrop={ondrop}
-                        ondragover={Callback::from(|event: DragEvent| {
-                        event.prevent_default();
-                        })}
-                        ondragenter={Callback::from(|event: DragEvent| {
-                            event.prevent_default();
-                        })}
-                    >
-                        <i class="fa fa-cloud-upload"></i>
-                        <p>{"Drop your files here or click to select"}</p>
-                    </div>
-                </label>
-                
-                <input
-                    id="file-upload"
-                    type="file"
-                    accept=".tsv,.csv"
-                    multiple={false}
-                    {onchange}
-                />
-                <div>
-                    <button {onclick} >{"Upload"}</button>
-                    <button onclick ={clear_file}>{"clear selected file"}</button>
-                </div>
-                <div id="preview-area">
-                    { for self.files.iter().map(Self::view_file) }
-                </div>
-            </div>
-        }
-    }
-}
-
-//---------------------------------------------------------------------------------------------------------
-
-impl FileUploader {
-    fn view_file(file: &FileDetails) -> Html {
-        html! {
-            <div class="preview-tile">
-                <p class="preview-name">{ format!("{}", file.name) }</p>
-                <div>
-                    {format!("{}", &file.data)}
-                </div>
-                
-            </div>
-        }
-    }
-
-    fn on_file_change(files: Option<FileList>) -> Msg {
+    let on_file_change = {
+        let files = files.clone();
+        Callback::from(move |file_list: Option<FileList>| {
+        let files = files.clone();
         let mut result = Vec::new();
 
-        if let Some(files) = files {
-            let files = js_sys::try_iter(&files)
+        if let Some(file_list) = file_list {
+            let file_list = js_sys::try_iter(&file_list)
                 .unwrap()
                 .unwrap()
                 .map(|v| web_sys::File::from(v.unwrap()))
                 .map(File::from);
-            result.extend(files);
+            result.extend(file_list);
         }
-        //log::debug!("file result{:?}",result);
-        Msg::Files(result)
+        files.set(result);
+    })};
+
+    let read_file_cb = {
+        let file_data = file_data.clone();
+        let readers = readers.clone();
+        Callback::from(move |file_details: FileDetails| {
+            file_data.set(Some(file_details));
+            readers.set(None);  
+    })};
+
+    {
+        let readers = readers.clone();
+        use_effect_with_deps(move |files| {
+            if !files.is_empty() {
+                let file = files[0].clone();
+                let file_name = file.name();
+
+                let mut result = vec![];
+                let task = {gloo::file::callbacks::read_as_text(&file, move |res| {
+                    read_file_cb.emit(FileDetails {
+                        name: file_name,
+                        data: res.expect("failed to read file"),
+                    });
+                })};
+                result.push(task);
+                readers.set(Some(result)); //Must hold onto reader for inner value to evaluate 
+            }
+        }, files.clone());
     }
 
-    fn upload_file(files: Vec<FileDetails>, upload_file_cb: Callback<AttrValue>) {
-        spawn_local(async move {
-            //log::debug!("in the async section now\n\n");
-            let mut file_json = HashMap::new();
-            let files = files.to_owned();
-            file_json.insert("name", &files[0].name);
-            file_json.insert( "data", &files[0].data);
+    let submit_file = {
+        let file_data = file_data.clone();
+        let props = props.clone();
+        Callback::from(move |_| {
+            let file_data = file_data.clone();
+            let props = props.clone();
+            spawn_local(async move {
+                if (*file_data).is_some() {
+                    log::debug!("file being sent to server");
 
-            let client = reqwest::Client::new();
-            //TODO fix to use a url variable instead of hard coded value
-            let server_res = client.post("http://localhost:8080/api/fileData")
-                .json(&file_json)
-                .send()
-                .await
-                .unwrap();
-            let res_body = server_res.text().await.unwrap();
-            upload_file_cb.emit(AttrValue::from(res_body));
-        });
+                    let mut file_json:HashMap<&str, &str> = HashMap::new();
+                    let file = (*file_data).to_owned().unwrap();
+                    file_json.insert("name", &file.name);
+                    file_json.insert( "data", &file.data);
+
+                    let client = reqwest::Client::new();
+                    //TODO fix to use a url variable instead of hard coded value
+                    let server_res = client.post("http://localhost:8080/api/fileData")
+                        .json(&file_json)
+                        .send()
+                        .await
+                        .unwrap();
+                    let res_body = server_res.text().await.unwrap();
+                    log::debug!("res_body: {}", res_body);
+                    let parsed_body: RecievedData = serde_json::from_str(&res_body).unwrap(); 
+                    props.sentences_cb.emit(parsed_body);
+                }
+            });
+        })
+    };
+    
+    html! {
+        <div id="wrapper">
+                
+            <p id="title">{ "Upload Your Files To The Cloud" }</p>
+                
+            <label for="file-upload">
+                <div
+                    id="drop-container"
+                    ondrop={ 
+                        let on_file_change = on_file_change.clone();
+                        Callback::from(move |event: DragEvent| {
+                            event.prevent_default();
+                            let event_files = event.data_transfer().unwrap().files();
+                            on_file_change.emit(event_files);
+                    })}
+                    ondragover={Callback::from(|event: DragEvent| {
+                    event.prevent_default();
+                    })}
+                    ondragenter={Callback::from(|event: DragEvent| {
+                        event.prevent_default();
+                    })}
+                >
+                    <i class="fa fa-cloud-upload"></i>
+                    <p>{"Drop your files here or click to select"}</p>
+                </div>
+            </label>
+                
+            <input
+                id="file-upload"
+                type="file"
+                accept=".tsv,.csv"
+                multiple={false}
+                onchange={Callback::from(move |e: Event| {
+                    let input: HtmlInputElement = e.target_unchecked_into(); //Option<FileList>
+                    on_file_change.emit(input.files());
+                })}
+            />
+            <div>
+                <button onclick={submit_file} >{"Upload"}</button>
+                <button onclick ={
+                    let files = files.clone();
+                    let file_data = file_data.clone();
+                    let readers = readers.clone();
+                    Callback::from(move |_| {
+                        files.set(vec![]);
+                        file_data.set(None);
+                        readers.set(None);
+                })}>{"clear selected file"}</button>
+            </div>
+            if let Some(file_data) = (*file_data).clone() {
+            <div id="preview-area">
+                { view_file(&file_data.clone()) }
+            </div>
+            }
+        </div>
     }
 }
 
+fn view_file(file: &FileDetails) -> Html {
+    html! {
+        <div class="preview-tile">
+            <p class="preview-name">{ format!("{}", file.name) }</p>
+            <div>
+                {format!("{}", &file.data)}
+            </div>
+        </div>
+    }
+}
--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,6 +1,12 @@
 use yew::prelude::*;
 use yew_router::prelude::*;
-use components::file_uploader::FileUploader;
+//use yew::Properties;
+use components::file_uploader::{
+    Entry,
+    FileUploader,
+    RecievedData,
+};
+//use components::file_uploader2::FileUploader2;
 
 mod components;
 
@@ -34,12 +40,60 @@ fn main() {
 
 #[function_component(Home)]
 fn home() -> Html {
+    let sent_from_file_uploader = use_state(RecievedData::default);
+    let sentences_cb = {
+        let sent_from_file_uploader = sent_from_file_uploader.clone();
+        Callback::from(move |data: RecievedData|{
+            log::debug!("sent from file uploader component: {:?}", data.sentence);
+            sent_from_file_uploader.set(data);
+    })};
 
+    /*
+    let sentences = vec![
+        Sentence {
+            id: 1,
+            text: "안녕하세요 테스트입니다".to_string(),
+        },
+        Sentence {
+            id: 2,
+            text: "안녕하세요, 이번 테스트입니다".to_string(),
+        }
+    ];
+    */
     html! {
         <div>
             <h1>{"korean dictionary tool"}</h1>
-            <FileUploader></FileUploader>
-
+            //<FileUploader sentences={sentences.clone()}/>
+            <FileUploader {sentences_cb}/>
+            <div>
+            {
+                (*sent_from_file_uploader).clone().sentence.into_iter().map(|entry: Entry|{
+                    html!{<div>{ format!("entry: word: {}, definition: {:?}, explaination: {:?}", entry.word, entry.definition, entry.explaination) }</div>}
+                }).collect::<Html>()
+            }
+            //<SentenceList sentences={sentences.clone()}/>
+            </div>
         </div>
     }
 }
+
+/*
+#[derive(Clone, PartialEq)]
+pub struct Sentence {
+    id: usize,
+    text: String,
+}
+
+#[derive(Clone, PartialEq, Properties)]
+pub struct SentenceListProps {
+    sentences: Vec<Sentence>,
+}
+*/
+/*
+#[function_component(SentenceList)]
+pub fn sentence_list(SentenceListProps {sentences} : &SentenceListProps) -> Html {
+    sentences.iter().map(|sentence| html! {
+        <p>{format!("{}: {}", sentence.id, sentence.text)}</p>
+    }).collect()
+}
+*/
